c6d79f8fa10db17dad9bbfdd1e2039148c2621aa
erdivyang10 <erdivyang10@gmail.com>
2018-05-23 18:19:31 +0530

    Add an object file

---
+++ 01intermediate/object.js
@@ -0,0 +1,12 @@
+// Learn to create an object
+
+let myPaper = {
+    paperTitle: 'License Plate recognition using Histogram Approach',
+    paperAuthor: 'Divynang Goswami',
+    publishYear: 2014,
+    Journal:'IJRITCC',
+    course: 'M.Tech In Digital Communications'
+
+}
+
+console.log(`Your paper is published on ${myPaper.publishYear} and author is ${myPaper.paperAuthor}`)
